6bc714467a1bd4a869d617c7b6a8ca9ed91953e4
Ram Sankar <mailtoramsankar@gmail.com>
2018-09-29 17:29:23 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -72,7 +72,7 @@ app.get('/add', (req,res) =>{
 
 
 
-app.get('/app-waterlevel', (req,res) =>{   
+app.post('/app-waterlevel', (req,res) =>{   
   MongoClient.connect(url, function(err, db) {
           if (err) throw err;
           var dbo = db.db("waterutil");                 
